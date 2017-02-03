466b03c034b45d3c06ac77cf2d0cb0d0a052702f
Hunar <sekhonhunar95@gmail.com>
2017-02-02 18:30:55 -0800

    Animation of game

--- app/src/main/java/com/example/hunar/ballonpopping/MainActivity.java
+++ app/src/main/java/com/example/hunar/ballonpopping/MainActivity.java
@@ -1,27 +1,77 @@
 package com.example.hunar.ballonpopping;
 
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewGroup mContentView;
+
+    private int[] mBalloonColours = new int[3];
+
+    private  int mNextColour, mScreenWidth, mScreenHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mBalloonColours[0] = Color.argb(255, 255, 0, 0);
+        mBalloonColours[0] = Color.argb(255, 0, 255, 0);
+        mBalloonColours[0] = Color.argb(255, 0, 0, 255);
+
         getWindow().setBackgroundDrawableResource(R.drawable.modern_background);
 
         mContentView = (ViewGroup) findViewById(R.id.activity_main);
+
+        setToFullScreen();
+
+        ViewTreeObserver viewTreeObserver = mContentView.getViewTreeObserver();
+        if(viewTreeObserver.isAlive()){
+            viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+                @Override
+                public void onGlobalLayout() {
+                    mContentView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    mScreenWidth = mContentView.getWidth();
+                    mScreenHeight = mContentView.getHeight();
+                }
+            });
+        }
 
         mContentView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 setToFullScreen();
+            }
+        });
+
+        mContentView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    Balloon b = new Balloon(MainActivity.this, mBalloonColours[mNextColour], 100);
+                    b.setX(event.getX());
+                    b.setY(mScreenHeight);
+                    mContentView.addView(b);
+                    b.releaseBallon(mScreenHeight, 3000);
+
+                    if(mNextColour + 1 == mBalloonColours.length){
+                        mNextColour = 0;
+                    }
+                    else{
+                        mNextColour++;
+                    }
+                }
+
+                return false;
             }
         });
     }
